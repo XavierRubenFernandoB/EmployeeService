f12117db922d8486d9f9f45dbe11c57c6f92c36a
Xavier Fernando <xavier.r.fernando@hotmail.com>
2020-06-07 17:20:09 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [Authorize]
+        public IEnumerable<Employee> GetEmployees()
+        {
+            using (DevDBEntities entities = new DevDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
